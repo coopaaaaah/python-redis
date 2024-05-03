109142c3cbb8f16168e30dcec2ac214ed893d731
David Cooper <david.cooper@unit21.ai>
2024-05-03 16:59:55 -0700

    10 -> 100000

--- server/main.py
+++ server/main.py
@@ -3,8 +3,6 @@ import json
 
 r = redis.Redis(host='cache', port=6379, db=0)
 
-
-r = redis.Redis(host='cache', port=6379, db=0)
 with open("/var/data.txt", "r") as f:
     for l in f.readlines():
         print(json.loads(r.get(l.strip())))
--- utils/main.py
+++ utils/main.py
@@ -2,7 +2,7 @@ import uuid
 
 file = open("data.txt", "w")
 
-for i in range(10):
+for i in range(100000):
     file.write("{}\n".format(uuid.uuid4()))
 
 file.close()
