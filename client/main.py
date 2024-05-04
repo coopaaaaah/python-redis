5591a3253258290819d13ba531f297436f9a7fab
David Cooper <david.cooper@unit21.ai>
2024-05-03 17:29:13 -0700

    added time checks for read / write speeds

--- client/main.py
+++ client/main.py
@@ -1,8 +1,22 @@
 import redis
 import random
 import json
+from datetime import datetime
+from enum import Enum
+
+class Type(Enum):
+    TRANSACTION = "TRANSACTION"
+    ACTION = "ACTION"
 
 r = redis.Redis(host='cache', port=6379, db=0)
+start_time = datetime.now()
+counter = 0
 with open("/var/data.txt", "r") as f:
     for l in f.readlines():
-        r.set(l.strip(), json.dumps({ "amount": random.randint(0, 10000) }))
+        r.set(l.strip(), json.dumps({ "amount": random.randint(0, 10000),"type": random.choice(list(Type)).value }))
+        counter += 1
+t = datetime.now() - start_time
+
+print(f"total records: {counter}")
+print(f"total write time: {t.microseconds / 1000}ms")
+print(f"avg write time: {t.microseconds / 1000 / counter}ms")
--- server/main.py
+++ server/main.py
@@ -1,8 +1,17 @@
 import redis
-import json
+from datetime import datetime
 
 r = redis.Redis(host='cache', port=6379, db=0)
 
+start_time = datetime.now()
+counter = 0
 with open("/var/data.txt", "r") as f:
     for l in f.readlines():
-        print(json.loads(r.get(l.strip())))
+        r.get(l.strip())
+        counter += 1
+
+t = datetime.now() - start_time
+
+print(f"total records: {counter}")
+print(f"total read time: {t.microseconds / 1000}ms")
+print(f"avg read time: {t.microseconds / 1000 / counter}ms")
