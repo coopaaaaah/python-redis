7f86348f3ba196282e1a772ea21ad7c95f020bbd
David Cooper <david.cooper@unit21.ai>
2024-05-30 09:37:07 -0700

    sorted set experiment

--- client/main.py
+++ client/main.py
@@ -1,22 +1,37 @@
 import redis
 import random
-import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from enum import Enum
-
+import time
+import random
+import os
 class Type(Enum):
-    TRANSACTION = "TRANSACTION"
-    ACTION = "ACTION"
+    BARTER_PAYOUT = "BARTER_PAYOUT"
+    F4B_PAYOUT_BANK_TRANSFER = "F4B_PAYOUT_BANK_TRANSFER"
+    ACQUIRED_COM_CARD = "ACQUIRED_COM_CARD"
+    PAYPAL = "PAYPAL"
+    ANDROID_POS = "ANDROID_POS"
+    FAWRY_PAY = "FAWRY_PAY"
+    F4B_PAYOUT_WALLET = "F4B_PAYOUT_WALLET"
+    F4R = "F4R"
+    WITHDRAWAL = "WITHDRAWAL"
+    MVISA_QR = "MVISA_QR"
+    BARTER_TO_BARTER = "BARTER_TO_BARTER"
+    MOBILEMONEY = "MOBILEMONEY"
+    VOUCHER = "1VOUCHER"
+    FLUTTERWAVE = "FLUTTERWAVE"
+    CPOS_TERMINAL = "CPOS_TERMINAL"
+    APPLEPAY = "APPLEPAY"
+    CRYPTOCURRENCY = "CRYPTOCURRENCY"
+    BARTER_PAYMENT = "BARTER_PAYMENT"
+    BANKREFERENCE = "BANKREFERENCE"
 
 r = redis.Redis(host='cache', port=6379, db=0)
-start_time = datetime.now()
-counter = 0
-with open("/var/data.txt", "r") as f:
-    for l in f.readlines():
-        r.set(l.strip(), json.dumps({ "amount": random.randint(0, 10000),"type": random.choice(list(Type)).value }))
-        counter += 1
-t = datetime.now() - start_time
+KEY = os.getenv("KEY")
 
-print(f"total records: {counter}")
-print(f"total write time: {t.microseconds / 1000}ms")
-print(f"avg write time: {t.microseconds / 1000 / counter}ms")
+for v in range(100000):
+    expiration = datetime.now() - timedelta(hours=0, minutes=1)
+    value = {random.choice(list(Type)).value: time.mktime(expiration.timetuple())}
+    print(f"{value} coming in...")
+    r.zadd(KEY, value, gt=True)
+    time.sleep(random.randint(1, 5))    
--- server/main.py
+++ server/main.py
@@ -1,17 +1,15 @@
 import redis
-from datetime import datetime
+import  time
+import os
+from datetime import datetime, timedelta
 
 r = redis.Redis(host='cache', port=6379, db=0)
+KEY = os.getenv("KEY")
 
-start_time = datetime.now()
-counter = 0
-with open("/var/data.txt", "r") as f:
-    for l in f.readlines():
-        r.get(l.strip())
-        counter += 1
-
-t = datetime.now() - start_time
-
-print(f"total records: {counter}")
-print(f"total read time: {t.microseconds / 1000}ms")
-print(f"avg read time: {t.microseconds / 1000 / counter}ms")
+for v in range(100000):
+    expiration = datetime.now() - timedelta(hours=0, minutes=1)
+    end = time.mktime(expiration.timetuple())
+    start = time.mktime(datetime.now().timetuple())
+    v = r.zrange(KEY, int(start), int(end))
+    print(f"{KEY} values in last 5 seconds: {v}")
+    time.sleep(5)
